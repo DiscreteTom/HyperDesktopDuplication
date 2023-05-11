074692f7b073377da23e2946dc4e900c3560591c
DiscreteTom <discrete_tom@outlook.com>
2023-05-12 01:03:24 +0800

    fix: CreateTexture2D

--- backend/src/duplicate_context.rs
+++ backend/src/duplicate_context.rs
@@ -5,12 +5,11 @@ use windows::{
   Win32::Graphics::{
     Direct3D11::{
       ID3D11Device, ID3D11DeviceContext, ID3D11Resource, ID3D11Texture2D, D3D11_BIND_FLAG,
-      D3D11_CPU_ACCESS_READ, D3D11_RESOURCE_MISC_FLAG, D3D11_SUBRESOURCE_DATA,
-      D3D11_TEXTURE2D_DESC, D3D11_USAGE_STAGING,
+      D3D11_CPU_ACCESS_READ, D3D11_RESOURCE_MISC_FLAG, D3D11_TEXTURE2D_DESC, D3D11_USAGE_STAGING,
     },
     Dxgi::{
-      IDXGIOutput1, IDXGIOutputDuplication, IDXGIResource, IDXGISurface1, DXGI_MAP_READ,
-      DXGI_OUTDUPL_FRAME_INFO, DXGI_OUTPUT_DESC, DXGI_RESOURCE_PRIORITY_MAXIMUM,
+      Common::DXGI_FORMAT_B8G8R8A8_UNORM, IDXGIOutput1, IDXGIOutputDuplication, IDXGISurface1,
+      DXGI_MAP_READ, DXGI_OUTDUPL_FRAME_INFO, DXGI_OUTPUT_DESC, DXGI_RESOURCE_PRIORITY_MAXIMUM,
     },
   },
 };
@@ -48,29 +47,35 @@ impl DuplicateContext {
     }
   }
 
-  pub fn acquire_next_frame(&self) -> IDXGISurface1 {
+  pub fn acquire_next_frame(&self, width: u32, height: u32) -> IDXGISurface1 {
     unsafe {
-      let frame_info = DXGI_OUTDUPL_FRAME_INFO::default();
-      let resource: Option<IDXGIResource> = None.clone();
+      let mut frame_info = DXGI_OUTDUPL_FRAME_INFO::default();
+      let mut resource: Option<ID3D11Texture2D> = None.clone();
       self
         .output_duplication
         .AcquireNextFrame(
           self.timeout_ms,
-          &frame_info as *const _ as *mut _,
-          &resource as *const _ as *mut _,
+          &mut frame_info as *mut _,
+          &mut resource as *const _ as *mut _,
         )
         .unwrap();
 
-      let resource = resource.unwrap();
-      let texture = (&resource as *const IDXGIResource).cast::<ID3D11Texture2D>();
+      let texture = resource.unwrap();
 
       // Configure the description to make the texture readable
       let mut texture_desc = D3D11_TEXTURE2D_DESC::default();
-      (*texture).GetDesc(&mut texture_desc as *mut _);
+      texture.GetDesc(&mut texture_desc as *mut _);
       texture_desc.BindFlags = D3D11_BIND_FLAG::default();
       texture_desc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
       texture_desc.MiscFlags = D3D11_RESOURCE_MISC_FLAG::default();
       texture_desc.Usage = D3D11_USAGE_STAGING; // A resource that supports data transfer (copy) from the GPU to the CPU.
+      texture_desc.Width = width;
+      texture_desc.Height = height;
+      texture_desc.MipLevels = 1;
+      texture_desc.ArraySize = 1;
+      texture_desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+      texture_desc.SampleDesc.Count = 1;
+      texture_desc.SampleDesc.Quality = 0;
 
       // copy a readable version of the texture in GPU
       let readable_texture: Option<ID3D11Texture2D> = None.clone();
@@ -90,21 +95,21 @@ impl DuplicateContext {
       let readable_surface = readable_texture.cast::<ID3D11Resource>().unwrap();
       self
         .device_context
-        .CopyResource(&readable_surface, &*texture);
+        .CopyResource(&readable_surface, &texture);
       self.output_duplication.ReleaseFrame().unwrap();
 
       readable_surface.cast().unwrap()
     }
   }
 
-  pub fn capture_frame(&self, dest: *mut u8, size: usize) {
+  pub fn capture_frame(&self, dest: *mut u8, width: u32, height: u32) {
     unsafe {
-      let frame = &self.acquire_next_frame();
+      let frame = &self.acquire_next_frame(width, height);
       let mapped_surface = ptr::null_mut();
       frame.Map(mapped_surface, DXGI_MAP_READ).unwrap();
       let mapped_surface = Box::from_raw(mapped_surface);
 
-      ptr::copy_nonoverlapping(mapped_surface.pBits, dest, size);
+      ptr::copy_nonoverlapping(mapped_surface.pBits, dest, (width * height * 4) as usize); // 4 for BGRA
 
       frame.Unmap().unwrap();
     }
