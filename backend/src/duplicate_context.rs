28d3413108ba363f61c8251b2629b7fc42a93ba6
DiscreteTom <discrete_tom@outlook.com>
2023-05-11 22:09:39 +0800

    fix: DupCtx.acquire_next_frame

--- backend/src/duplicate_context.rs
+++ backend/src/duplicate_context.rs
@@ -5,11 +5,12 @@ use windows::{
   Win32::Graphics::{
     Direct3D11::{
       ID3D11Device, ID3D11DeviceContext, ID3D11Resource, ID3D11Texture2D, D3D11_BIND_FLAG,
-      D3D11_CPU_ACCESS_READ, D3D11_RESOURCE_MISC_FLAG, D3D11_USAGE_STAGING,
+      D3D11_CPU_ACCESS_READ, D3D11_RESOURCE_MISC_FLAG, D3D11_SUBRESOURCE_DATA,
+      D3D11_TEXTURE2D_DESC, D3D11_USAGE_STAGING,
     },
     Dxgi::{
-      IDXGIOutput1, IDXGIOutputDuplication, IDXGISurface1, DXGI_MAP_READ, DXGI_OUTPUT_DESC,
-      DXGI_RESOURCE_PRIORITY_MAXIMUM,
+      IDXGIOutput1, IDXGIOutputDuplication, IDXGIResource, IDXGISurface1, DXGI_MAP_READ,
+      DXGI_OUTDUPL_FRAME_INFO, DXGI_OUTPUT_DESC, DXGI_RESOURCE_PRIORITY_MAXIMUM,
     },
   },
 };
@@ -49,31 +50,39 @@ impl DuplicateContext {
 
   pub fn acquire_next_frame(&self) -> IDXGISurface1 {
     unsafe {
-      let resource = ptr::null_mut();
-      let frame_info = ptr::null_mut();
+      let frame_info = DXGI_OUTDUPL_FRAME_INFO::default();
+      let resource: Option<IDXGIResource> = None.clone();
       self
         .output_duplication
-        .AcquireNextFrame(self.timeout_ms, frame_info, resource)
+        .AcquireNextFrame(
+          self.timeout_ms,
+          &frame_info as *const _ as *mut _,
+          &resource as *const _ as *mut _,
+        )
         .unwrap();
 
-      // let frame_info = Box::from_raw(frame_info);
-      let texture = Box::from_raw(resource.cast::<Option<ID3D11Texture2D>>()).unwrap();
+      let resource = resource.unwrap();
+      let texture = (&resource as *const IDXGIResource).cast::<ID3D11Texture2D>();
 
       // Configure the description to make the texture readable
-      let mut texture_desc = ptr::null_mut();
-      texture.GetDesc(texture_desc);
-      (*texture_desc).BindFlags = D3D11_BIND_FLAG(0);
-      (*texture_desc).CPUAccessFlags = D3D11_CPU_ACCESS_READ;
-      (*texture_desc).MiscFlags = D3D11_RESOURCE_MISC_FLAG(0);
-      (*texture_desc).Usage = D3D11_USAGE_STAGING; // A resource that supports data transfer (copy) from the GPU to the CPU.
+      let mut texture_desc = D3D11_TEXTURE2D_DESC::default();
+      (*texture).GetDesc(&mut texture_desc as *mut _);
+      texture_desc.BindFlags = D3D11_BIND_FLAG(0);
+      texture_desc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
+      texture_desc.MiscFlags = D3D11_RESOURCE_MISC_FLAG(0);
+      texture_desc.Usage = D3D11_USAGE_STAGING; // A resource that supports data transfer (copy) from the GPU to the CPU.
 
       // copy a readable version of the texture in GPU
-      let readable_texture = ptr::null_mut();
+      let readable_texture: Option<ID3D11Texture2D> = None.clone();
       self
         .device
-        .CreateTexture2D(texture_desc, None, Some(readable_texture))
+        .CreateTexture2D(
+          &texture_desc,
+          None,
+          Some(&readable_texture as *const _ as *mut _),
+        )
         .unwrap();
-      let readable_texture = Box::from_raw(readable_texture).unwrap();
+      let readable_texture = readable_texture.unwrap();
       // Lower priorities causes stuff to be needlessly copied from gpu to ram,
       // causing huge ram usage on some systems.
       // https://github.com/bryal/dxgcap-rs/blob/208d93368bc64aed783791242410459c878a10fb/src/lib.rs#L225
@@ -81,7 +90,7 @@ impl DuplicateContext {
       let readable_surface = readable_texture.cast::<ID3D11Resource>().unwrap();
       self
         .device_context
-        .CopyResource(&readable_surface, &texture);
+        .CopyResource(&readable_surface, &*texture);
       self.output_duplication.ReleaseFrame().unwrap();
 
       readable_surface.cast().unwrap()
