6c3246b6968c42842c8ee3b590bb7d611750a581
DiscreteTom <discrete_tom@outlook.com>
2023-05-13 20:44:18 +0800

    chore: optimize code

--- backend/src/handler.rs
+++ backend/src/handler.rs
@@ -2,7 +2,7 @@ use std::convert::Infallible;
 
 use crate::{
   manager::Manager,
-  model::{DisplayInfo, DisplaysInfo},
+  model::{DisplaysInfo, DxgiOutputDescExt},
 };
 
 pub async fn list_displays(manager: Manager) -> Result<impl warp::Reply, Infallible> {
@@ -13,14 +13,7 @@ pub async fn list_displays(manager: Manager) -> Result<impl warp::Reply, Infalli
       .iter()
       .map(|ctx| {
         let desc = ctx.desc().unwrap();
-        DisplayInfo {
-          bottom: desc.DesktopCoordinates.bottom,
-          top: desc.DesktopCoordinates.top,
-          left: desc.DesktopCoordinates.left,
-          right: desc.DesktopCoordinates.right,
-          name: String::from_utf16_lossy(&desc.DeviceName),
-          rotation: desc.Rotation.0,
-        }
+        desc.to_info()
       })
       .collect(),
   }))
--- backend/src/model.rs
+++ backend/src/model.rs
@@ -1,4 +1,5 @@
 use serde_derive::Serialize;
+use windows::Win32::Graphics::Dxgi::DXGI_OUTPUT_DESC;
 
 #[derive(Serialize, Clone, Debug)]
 pub struct DisplaysInfo {
@@ -13,4 +14,21 @@ pub struct DisplayInfo {
   pub right: i32,
   pub name: String,
   pub rotation: i32,
+}
+
+pub trait DxgiOutputDescExt {
+  fn to_info(&self) -> DisplayInfo;
+}
+
+impl DxgiOutputDescExt for DXGI_OUTPUT_DESC {
+  fn to_info(&self) -> DisplayInfo {
+    DisplayInfo {
+      bottom: self.DesktopCoordinates.bottom,
+      top: self.DesktopCoordinates.top,
+      left: self.DesktopCoordinates.left,
+      right: self.DesktopCoordinates.right,
+      name: String::from_utf16_lossy(&self.DeviceName),
+      rotation: self.Rotation.0,
+    }
+  }
 }
