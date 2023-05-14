7987cde44cbb6b358ec858be775b4598b8774e0a
DiscreteTom <discrete_tom@outlook.com>
2023-05-14 22:17:17 +0800

    feat: parse port from args

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,17 +4,33 @@ mod server;
 
 use crate::{manager::manager_thread, server::server_thread};
 use model::ServerMutex;
-use std::sync::Arc;
+use std::{env::args, sync::Arc};
 use tokio::sync::{mpsc, Mutex};
 
 #[tokio::main]
 async fn main() {
+  let args: Vec<String> = args().collect();
+
+  if args.len() != 2 {
+    println!("Usage: {} <port>", args[0]);
+    return;
+  }
+
+  // parse port from args
+  let port: u16 = match args[1].parse() {
+    Ok(port) => port,
+    Err(_) => {
+      println!("Invalid port: {}", args[1]);
+      return;
+    }
+  };
+
   let mutex: ServerMutex = Arc::new(Mutex::new(()));
   let (tx, rx) = mpsc::channel(1);
 
   tokio::spawn(async move {
-    println!("running on localhost:3030");
-    server_thread(mutex, tx).await;
+    println!("running on localhost:{}", port);
+    server_thread(mutex, tx, port).await;
   });
 
   manager_thread(rx).await;
--- backend/src/server.rs
+++ backend/src/server.rs
@@ -4,8 +4,8 @@ pub mod route;
 use self::route::all_routes;
 use crate::model::{ActionSender, ServerMutex};
 
-pub async fn server_thread(mutex: ServerMutex, tx: ActionSender) {
+pub async fn server_thread(mutex: ServerMutex, tx: ActionSender, port: u16) {
   warp::serve(all_routes(mutex, tx))
-    .run(([127, 0, 0, 1], 3030))
+    .run(([127, 0, 0, 1], port))
     .await;
 }
