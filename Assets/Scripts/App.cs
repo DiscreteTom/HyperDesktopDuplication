e0bc8d53bad80a3d9ff6848747b23227587c8f63
DiscreteTom <discrete_tom@outlook.com>
2023-05-21 13:48:24 +0800

    chore: move App to HDD namespace

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Runtime.InteropServices;
-using HyperDesktopDuplication;
 using UnityEngine;
 
-public class App : MonoBehaviour {
-  async void Start() {
-    var manager = this.GetComponent<HDD_Manager>();
-    await manager.Refresh();
-    manager.CreateMonitor(0);
+namespace HyperDesktopDuplication {
+  public class App : MonoBehaviour {
+    async void Start() {
+      var manager = this.GetComponent<HDD_Manager>();
+      await manager.Refresh();
+      manager.CreateMonitor(0);
+    }
   }
 }
