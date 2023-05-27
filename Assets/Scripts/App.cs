f7f0be0c1abd716e1535b28b5b19e143b563c5ed
DiscreteTom <discrete_tom@outlook.com>
2023-05-27 16:52:54 +0800

    chore: change HDD_Monitor's scale to 1

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -2,24 +2,29 @@ using UnityEngine;
 
 namespace HyperDesktopDuplication {
   public class App : MonoBehaviour {
+    const float scale = 100;
+
     async void Start() {
       var manager = this.GetComponent<HDD_Manager>();
       await manager.Refresh();
 
+      // get the center position of the primary monitor
       var primaryCenter = Vector3.zero;
       for (int i = 0; i < manager.Monitors.Count; ++i) {
         var info = manager.Monitors[i];
         if (info.IsPrimary) {
-          primaryCenter = new Vector3((info.Right - info.Left) / 2 + info.Left, (info.Top - info.Bottom) / 2 + info.Bottom, 0) / 100;
+          primaryCenter = new Vector3((info.Right - info.Left) / 2 + info.Left, (info.Top - info.Bottom) / 2 + info.Bottom, 0) / scale;
           break;
         }
       }
 
+      // create all monitors
       for (int i = 0; i < manager.Monitors.Count; ++i) {
         var info = manager.Monitors[i];
         print($"display {i}: left={info.Left}, right={info.Right}, top={info.Top}, bottom={info.Bottom}");
         var obj = manager.CreateMonitor(i);
-        obj.transform.localPosition = new Vector3((info.Right - info.Left) / 2 + info.Left, (info.Top - info.Bottom) / 2 + info.Bottom, 0) / 100 - primaryCenter;
+        obj.transform.localScale = new Vector3(1 / scale, 1 / scale, 1);
+        obj.transform.localPosition = new Vector3((info.Right - info.Left) / 2 + info.Left, (info.Top - info.Bottom) / 2 + info.Bottom, 0) / scale - primaryCenter;
       }
     }
   }
