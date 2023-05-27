f279f2c3872adbcb76746cfa02b16ca5920f2ab1
DiscreteTom <discrete_tom@outlook.com>
2023-05-27 16:48:11 +0800

    chore: update App, move primary monitor to center

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -6,11 +6,20 @@ namespace HyperDesktopDuplication {
       var manager = this.GetComponent<HDD_Manager>();
       await manager.Refresh();
 
+      var primaryCenter = Vector3.zero;
+      for (int i = 0; i < manager.Monitors.Count; ++i) {
+        var info = manager.Monitors[i];
+        if (info.IsPrimary) {
+          primaryCenter = new Vector3((info.Right - info.Left) / 2 + info.Left, (info.Top - info.Bottom) / 2 + info.Bottom, 0) / 100;
+          break;
+        }
+      }
+
       for (int i = 0; i < manager.Monitors.Count; ++i) {
         var info = manager.Monitors[i];
         print($"display {i}: left={info.Left}, right={info.Right}, top={info.Top}, bottom={info.Bottom}");
         var obj = manager.CreateMonitor(i);
-        obj.transform.localPosition = new Vector3((info.Right - info.Left) / 2 + info.Left, (info.Top - info.Bottom) / 2 + info.Bottom, 0) / 100;
+        obj.transform.localPosition = new Vector3((info.Right - info.Left) / 2 + info.Left, (info.Top - info.Bottom) / 2 + info.Bottom, 0) / 100 - primaryCenter;
       }
     }
   }
